0332740e9763537b2357ca8ec13e0c1bfff65826
Mantis <mjbgtaad56@gmail.com>
2026-04-17 13:22:23 -0400

    Implemented fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,14 +11,14 @@
 <Header />
 <section class="jumbotron">
 	<div>
-		<h1>
-			SolCorp
+		<div class="title">
+			<h1>SolCorp</h1>
 			<div class="underline" style="--id:1;"></div>
 			<div class="underline" style="--id:0.8;"></div>
 			<div class="underline" style="--id:0.6;"></div>
 			<div class="underline" style="--id:0.4;"></div>
 			<div class="underline" style="--id:0.2;"></div>
-		</h1>
+		</div>
 	</div>
 </section>
 <section>
@@ -36,6 +36,28 @@
 <Footer />
 
 <style lang="scss">
+	.title {
+		position: relative;
+		width: fit-content;
+		position: relative;
+		display: block;
+		transform: scaleX(110%);
+	}
+
+	.underline {
+		content: '';
+		display: block;
+		position: relative;
+		width: calc(40% + 30% * var(--id));
+		top: -1rem;
+		left: 50%;
+		translate: -50%;
+		height: calc(1px + 5px * var(--id));
+		margin-bottom: calc(8px - 5px * var(--id));
+		background-color: white;
+		opacity: calc(20% + 60% * var(--id));
+	}
+
 	.jumbotron {
 		padding: 7.5rem 0;
 
@@ -48,20 +70,9 @@
 			display: block;
 			width: fit-content;
 			position: relative;
-
-			.underline {
-				content: '';
-				display: block;
-				position: relative;
-				width: calc(40% + 30% * var(--id));
-				top: -1rem;
-				left: 50%;
-				translate: -50%;
-				height: calc(1px + 5px * var(--id));
-				margin-bottom: calc(8px - 5px * var(--id));
-				background-color: white;
-				opacity: calc(20% + 60% * var(--id));
-			}
+			text-transform: uppercase;
+			letter-spacing: 1rem;
+			padding-left: 1rem;
 		}
 	}
 </style>
